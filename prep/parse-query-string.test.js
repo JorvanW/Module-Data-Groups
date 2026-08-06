673f8859d15c2e108941873e61955259783c894e
JorvanW <your.email@example.com>
2026-08-06 11:08:47 +0100

    removed prep folder

--- prep/parse-query-string.test.js
+++
@@ -1,7 +0,0 @@
-test("given a query string with no query parameters, returns an empty object", function () {
-  const input = "";
-  const currentOutput = parseQueryString(input);
-  const targetOutput = {};
-
-  expect(currentOutput).toEqual(targetOutput);
-});
